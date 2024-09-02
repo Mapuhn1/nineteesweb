79a0b550b48186070ef25a585a2d6b0f3a91bd87
Hazel Mapuranga (1000125469) <MAPUHN1@student.op.ac.nz>
2024-09-03 11:17:24 +1200

    finishing functionality in js

--- Web forms/interactive.js
+++ Web forms/interactive.js
@@ -1,7 +1,7 @@
-
+// drag and 
 
 const draggables = document.querySelectorAll('.draggable');
-const dropZones = document.querySelectorAll('.drop-zone');
+const dropZones = document.querySelectorAll('.dropzone');
  
 draggables.forEach(draggable => {
     draggable.addEventListener('dragstart', dragStart);
@@ -25,4 +25,33 @@ function drop(event) {
     const data = event.dataTransfer.getData("text");
     event.target.textContent = document.getElementById(data).textContent;
     document.getElementById(data).style.display = "none";
+}
+
+// for questions 1 to 3
+let score = 0;
+let result = "";
+
+function checkAns(){
+    // Question 1
+    const q1 = document.querySelectorAll('input[name="q1"]:checked');
+    let q1Correct = ["Helium", "Oxygen"];
+    let q1UserAnswers = Array.from(q1).map(input => input.value);
+    if (arraysEqual(q1UserAnswers, q1Correct)) {
+        score++;
+    }
+
+    //Question 2
+    const q2 = document.querySelector('input[name="q2"]:checked');
+    if (q2 && q2.value === "Mars") {
+        score++;
+    }
+    //question3
+    const q3 = document.getElementsByTagName('q3').value.trim().toLowerCase();
+    if (q3 === "twiglight") {
+        score++;
+    }
+}
+function showResults(){
+    document.getElementById('result').textContent = `You scored ${score} out of 4.`;
+
 }
