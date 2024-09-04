3002896d99b5c3350c33cb72c3efdbc2888e1f43
Hazel Mapuranga (1000125469) <MAPUHN1@student.op.ac.nz>
2024-09-05 11:54:05 +1200

    sba practice

---
+++ JS Practice/sba practice/script.js
@@ -0,0 +1,35 @@
+// // append can append string and elements while append child cant append strings
+// const body = document.body
+// // body.append("Hello world");
+
+// // // appending elements
+// // const div = document.createElement("div");
+// // //div.innerText = "hello pookie"; 
+// // //or i can use 
+// // div.textContent = "Hello Bookie";
+// // body.append(div);
+
+// const div = document.querySelector("div");
+// const spanHi = document.querySelector("#hi");
+// const spanBye = document.querySelector("#bye")
+
+// //to remove a child element => since span is the child element of div
+// div.removeChild(spanBye)  // or spanBye.remove()
+// //to add the child back
+// div.appendChild(spanBye)
+
+// / event listeners
+
+const granpa = document.querySelector(".granpa");
+const parent = documnet.querySelector(".parent");
+const child = documnet.querySelector(".child");
+
+
+// event lsiteners take 2 parameters. fisrt the event youre listening for in my case "click" 
+// 2nd parmeter is the callback function after the click takes place
+granpa.addEventListener("click",  )
+
+function Hello(){
+    
+}
+
